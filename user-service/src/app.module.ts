01418e72f1aaa7af056ebe8e4b737561e182c9fd
Yesid11 <yesidrobayo11@gmail.com>
2025-03-02 17:34:07 -0500

    feat: refactor environment configuration files and scripts for user and order services

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -5,15 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { KafkaService } from './kafka/kafka.service';
 import { UserValidationService } from './user-validation/user-validation.service';
-import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, envFilePath: path.resolve(
-        process.cwd(),
-        `.env.${process.env.NODE_ENV || 'development'}`,
-      ),
+      isGlobal: true,
+      envFilePath: ['.env.development', '.env.production', 'env.test'],
+
     }),
     PrismaModule,
     UserModule,
